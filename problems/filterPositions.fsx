6099cd2eaa6f7e582f5fa3856dd4bf238b152234
Anjum219 <anjumhaz@gmail.com>
2022-01-04 22:11:38 +0600

    Add optimized solution for filterPosition

--- problems/filterPositions.fsx
+++ problems/filterPositions.fsx
@@ -1,13 +1,10 @@
 open System
 
-let rec filterPos (array: list<int>) (filteredArray: list<int>) (pos: int): list<int> =
-    if pos >= array.Length then
-        filteredArray
-    else
-        filterPos
-            array
-            (List.append filteredArray [array.[pos]])
-            (pos+2)
+let filterPos (array: list<int>): list<int> =
+    [for i = 0 to (array.Length-1) do 
+        if i%2 = 1 then
+            yield array.[i]
+    ]
 
 let array = [
     let mutable i = Console.ReadLine()
@@ -16,5 +13,5 @@ let array = [
         i <- Console.ReadLine()
 ]
 
-let filteredArray = filterPos array [] 1
+let filteredArray = filterPos array
 List.map (fun x -> printfn "%d" x) filteredArray
